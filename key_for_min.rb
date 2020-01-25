bc04d0cc3764225f1187cc8fb2a2fe2d08740f93
Brian Salemi <briandsalemi@gmail.com>
2020-01-25 21:31:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(hash)
   smallest_value = nil
   smallest_value_key = nil
   hash.each do |key, value|
-    if value < smallest_value
+    if smallest_value == nil || value < smallest_value
       smallest_value = value
       smallest_value_key = key
     end
