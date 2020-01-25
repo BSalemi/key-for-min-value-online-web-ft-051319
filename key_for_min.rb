ce7f99554d706b89543aac2a767f4eae95583e90
Brian Salemi <briandsalemi@gmail.com>
2020-01-25 21:28:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,12 @@ require 'pry'
 
 def key_for_min_value(hash)
   smallest_value = ""
+  smallest_value_key = ""
   hash.each do |key, value|
     if hash[key] < smallest_value
       smallest_value = hash[key]
+      smallest_value_key = key
     end
   end
+  return smallest
 end
